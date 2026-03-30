1ff234ffe9c7abc1648aac82960b4c9f25d8a206
ravindrakore <56753944-ravindrakore@users.noreply.replit.com>
2026-03-30 01:40:25 +0000

    Restored to '45fbd409cd615ead16d9eed430794f7694d3c514'
    Replit-Restored-To: 45fbd409cd615ead16d9eed430794f7694d3c514

--- src/AgriApp.Web/HttpServices/IAuthService.cs
+++
@@ -1,9 +0,0 @@
-using AgriApp.Web.Models;
-
-namespace AgriApp.Web.HttpServices;
-
-public interface IAuthService
-{
-    Task<LoginResponse?> LoginAsync(LoginRequest request);
-    Task LogoutAsync();
-}
